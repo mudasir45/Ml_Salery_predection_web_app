59e93ad9953a0d6214684f99f8e9802b90d3c34c
Mudassar Amin <mudasiramin456@gmail.com>
2023-07-07 16:22:42 +0500

    commit

--- web_app/main/views.py
+++ web_app/main/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+import model.ml
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        country = request.POST.get('country')
+        education = request.POST.get('education')
+        empLevel = request.POST.get('empLevel')
+        exp = request.POST.get('exp')
+
+        
+
     return render(request, 'index.html')
 
