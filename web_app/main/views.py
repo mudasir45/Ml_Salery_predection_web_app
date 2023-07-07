6187762ee4c8b5d9e978757a002499ce1127a6b4
Mudassar Amin <mudasiramin456@gmail.com>
2023-07-07 16:07:19 +0500

    commit

--- web_app/main/views.py
+++ web_app/main/views.py
@@ -3,3 +3,45 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+United States of America                                15040
+Other                                                    9740
+India                                                    6639
+Germany                                                  5395
+United Kingdom of Great Britain and Northern Ireland     4190
+Canada                                                   2490
+France                                                   2328
+Brazil                                                   2109
+Poland                                                   1732
+Netherlands                                              1555
+Spain                                                    1521
+Italy                                                    1514
+Australia                                                1462
+Russian Federation                                       1229
+Turkey                                                   1020
+Sweden                                                   1020
+Switzerland                                               932
+Austria                                                   833
+Israel                                                    795
+Iran, Islamic Republic of...                              723
+Pakistan                                                  711
+Czech Republic                                            692
+China                                                     632
+Belgium                                                   626
+Bangladesh                                                613
+Ukraine                                                   593
+Romania                                                   581
+Mexico                                                    574
+Portugal                                                  563
+Greece                                                    548
+Denmark                                                   547
+Indonesia                                                 537
+Argentina                                                 521
+Nigeria                                                   517
+South Africa                                              493
+Norway                                                    479
+Finland                                                   468
+Hungary                                                   466
+New Zealand                                               440
+Egypt                                                     400
