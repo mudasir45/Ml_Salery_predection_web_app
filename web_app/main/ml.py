2d36f12d688005ddc02c27bbf473678c1044ed70
Mudassar Amin <mudasiramin456@gmail.com>
2023-07-07 21:37:04 +0500

    commit

--- web_app/main/ml.py
+++ web_app/main/ml.py
@@ -1,14 +1,10 @@
 import pickle
+import os
 import numpy as np
 
 def load_model():
-    with open('Ml_Model.pkl', 'rb') as file:
+    file_path = os.path.join('main', 'Ml_Model.pkl')
+    with open(file_path, 'rb') as file:
         data = pickle.load(file)
     return data
 
-data = load_model()
-
-regressor = data["model"]
-le_education = data["le_education"]
-le_country = data["le_country"]
-le_emp = data["le_emp"]
--- web_app/main/views.py
+++ web_app/main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from . import ml 
+from .ml import load_model
 import numpy as np
 import pandas as pd
+
+
 
 # Create your views here.
 def home(request):
@@ -11,9 +13,26 @@ def home(request):
         empLevel = request.POST.get('empLevel')
         exp = request.POST.get('exp')
 
+        data = load_model()
+
+        regressor = data["model"]
+        le_education = data["le_education"]
+        le_country = data["le_country"]
+        le_emp = data["le_emp"]
         x = np.array([[country, education, exp, empLevel]])
 
-        test_df = pd.DataFrame(x, columns=['Country', 'EdLevel', 'YearsCodePro', 'Employment'])
+        df = pd.DataFrame(x, columns=['Country', 'EdLevel', 'YearsCodePro', 'Employment'])
+        df['EdLevel'] = le_education.transform(df['EdLevel'])
+        df['Country'] = le_country.transform(df['Country'])
+        df['Employment'] = le_emp.transform(df['Employment'])
+        df = df.astype(float)
+        
+        salary = regressor.predict(df)
+        salary = f"${salary[0]:.2f}"
+        context = {
+            'salary':salary,
+        }
+        return render(request, 'index.html', context)
 
     return render(request, 'index.html')
 
